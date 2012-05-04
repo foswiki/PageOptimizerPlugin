13303911ba0493306817231b86e5f3bd70d48932
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/PageOptimizerPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/PageOptimizerPlugin.pm
+++ lib/Foswiki/Plugins/PageOptimizerPlugin.pm
@@ -18,352 +18,381 @@ package Foswiki::Plugins::PageOptimizerPlugin;
 use strict;
 use warnings;
 
-use Foswiki::Func ();    # The plugins API
-use Foswiki::Plugins (); # For the API version
-use Digest::MD5 ();
-use URI ();
-use Compress::Zlib ();
+use Foswiki::Func    ();    # The plugins API
+use Foswiki::Plugins ();    # For the API version
+use Digest::MD5      ();
+use URI              ();
+use Compress::Zlib   ();
 
-our $VERSION = '$Rev$';
-our $RELEASE = '0.04';
-our $SHORTDESCRIPTION = 'Optimize html markup, as well as js and css';
+our $VERSION           = '$Rev$';
+our $RELEASE           = '0.04';
+our $SHORTDESCRIPTION  = 'Optimize html markup, as well as js and css';
 our $NO_PREFS_IN_TOPIC = 1;
-our $pluginName = 'PageOptimizerPlugin';
+our $pluginName        = 'PageOptimizerPlugin';
 
 use constant DEBUG => 0;    # toggle me
 
 ###############################
 sub initPlugin {
 
-  Foswiki::Func::registerRESTHandler('statistics', sub {
-    require Foswiki::Plugins::PageOptimizerPlugin::Stats;
-    return Foswiki::Plugins::PageOptimizerPlugin::Stats::restStatistics(@_);
-  });
+    Foswiki::Func::registerRESTHandler(
+        'statistics',
+        sub {
+            require Foswiki::Plugins::PageOptimizerPlugin::Stats;
+            return Foswiki::Plugins::PageOptimizerPlugin::Stats::restStatistics(
+                @_);
+        }
+    );
 
-  return 1;
+    return 1;
 }
 
 ###############################
 sub writeDebug {
-  return unless DEBUG;
-  print STDERR "- $pluginName - " . $_[0] . "\n";
+    return unless DEBUG;
+    print STDERR "- $pluginName - " . $_[0] . "\n";
 }
 
 ###############################
 sub completePageHandler {
-  my $text = $_[0];
-  my $header = $_[1];
+    my $text   = $_[0];
+    my $header = $_[1];
 
-  return unless $header =~ /Content-type: text\/html/;
+    return unless $header =~ /Content-type: text\/html/;
 
-  writeDebug("completePageHandler()");
+    writeDebug("completePageHandler()");
 
-  # clean up
-  if ($Foswiki::cfg{PageOptimizerPlugin}{CleanUpHTML}) {
-    $text =~ s/<!--.*?-->//g;
-    $text =~ s/^\s*$//gms;
-    $text =~ s/(<\/html>).*?$/$1/gs;
-    $text =~ s/<p><\/p>(?:\s*<p><\/p>)*/\n<p><\/p>\n/gs;
+    # clean up
+    if ( $Foswiki::cfg{PageOptimizerPlugin}{CleanUpHTML} ) {
+        $text =~ s/<!--.*?-->//g;
+        $text =~ s/^\s*$//gms;
+        $text =~ s/(<\/html>).*?$/$1/gs;
+        $text =~ s/<p><\/p>(?:\s*<p><\/p>)*/\n<p><\/p>\n/gs;
 
-    # clean up %{<verbatim>}% ...%{</verbatim>}%
-    $text =~ s/\%{(<pre[^>]*>)}&#37;\s*/$1/g;
-    $text =~ s/\s*&#37;{(<\/pre>)}\%/$1/g;
+        # clean up %{<verbatim>}% ...%{</verbatim>}%
+        $text =~ s/\%{(<pre[^>]*>)}&#37;\s*/$1/g;
+        $text =~ s/\s*&#37;{(<\/pre>)}\%/$1/g;
 
-    $text =~ s/<script +type=["']text\/javascript["']/<script/g;
-    $text =~ s/<style +type=["']text\/css["']/<style/g;
-    $text =~ s/<link (.*?rel=["']stylesheet["'].*?)\/>/_processLinkStyle($1)/ge;
-  }
+        $text =~ s/<script +type=["']text\/javascript["']/<script/g;
+        $text =~ s/<style +type=["']text\/css["']/<style/g;
+        $text =~
+          s/<link (.*?rel=["']stylesheet["'].*?)\/>/_processLinkStyle($1)/ge;
+    }
 
-  my $query = Foswiki::Func::getCgiQuery();
-  my $refresh = $query->param("refresh") || '';
-  purgeCache() if $refresh =~ /\ball\b/;
+    my $query = Foswiki::Func::getCgiQuery();
+    my $refresh = $query->param("refresh") || '';
+    purgeCache() if $refresh =~ /\ball\b/;
 
-  $text = optimizeJavaScript($text) if $Foswiki::cfg{PageOptimizerPlugin}{OptimizeJavaScript};
-  $text = optimizeStylesheets($text) if $Foswiki::cfg{PageOptimizerPlugin}{OptimizeStylesheets};
+    $text = optimizeJavaScript($text)
+      if $Foswiki::cfg{PageOptimizerPlugin}{OptimizeJavaScript};
+    $text = optimizeStylesheets($text)
+      if $Foswiki::cfg{PageOptimizerPlugin}{OptimizeStylesheets};
 
-  $text =~ s/^\s+$//gms;    # remove a few empty lines
+    $text =~ s/^\s+$//gms;    # remove a few empty lines
 
-  # return the changed text
-  $_[0] = $text;
+    # return the changed text
+    $_[0] = $text;
 }
 
 ###############################
 sub _processLinkStyle {
-  my $args = shift;
-  $args =~ s/type=["'].*?["']//g;
-  return "<link $args/>";
+    my $args = shift;
+    $args =~ s/type=["'].*?["']//g;
+    return "<link $args/>";
 }
-
 
 ###############################
 sub preRenderingHandler {
-  # better cite markup
-  $_[0] =~ s/[\n\r](>.*?)([\n\r][^>])/_processCite($1).$2/ges;
+
+    # better cite markup
+    $_[0] =~ s/[\n\r](>.*?)([\n\r][^>])/_processCite($1).$2/ges;
 }
 
 ###############################
 sub _processCite {
-  my $block = shift;
+    my $block = shift;
 
-  $block =~ s/^>/<span class='foswikiCiteChar'>&gt;<\/span>/gm;
-  $block =~ s/\n/<br \/>\n/g;
+    $block =~ s/^>/<span class='foswikiCiteChar'>&gt;<\/span>/gm;
+    $block =~ s/\n/<br \/>\n/g;
 
-  my $class = ($block =~ /<br \/>/)?'foswikiBlockCite':'foswikiCite';
+    my $class = ( $block =~ /<br \/>/ ) ? 'foswikiBlockCite' : 'foswikiCite';
 
-  return "<div class='$class'>".$block."</div>";
+    return "<div class='$class'>" . $block . "</div>";
 }
 
 ###############################
 sub optimizeJavaScript {
-  my $text = shift;
+    my $text = shift;
 
-  # take out ie stuff
-  my @ieConditionals = ();
-  my $index = 0;
-  while ($text =~ s/(<!\-\- +\[if.*?<!\[endif\]\-\->)/\0ie$index/s) {
+    # take out ie stuff
+    my @ieConditionals = ();
+    my $index          = 0;
+    while ( $text =~ s/(<!\-\- +\[if.*?<!\[endif\]\-\->)/\0ie$index/s ) {
 
-    writeDebug("found ie conditional: $1");
-    push @ieConditionals, $1;
-    $index++;
-  }
-
-  # collect all javascript
-  my @jsUrls = ();
-  my $excludePattern = '(?!.*'.$Foswiki::cfg{PageOptimizerPlugin}{ExcludeJavaScript}.')' 
-    if defined $Foswiki::cfg{PageOptimizerPlugin}{ExcludeJavaScript};
-
-  while ($text =~ s/<script .*?src=["'](\/$excludePattern[^"']+)["'].*><\/script>/\0js\0/) {
-    push @jsUrls, $1;
-  }
-
-  # put back ie stuff
-  $index = 0;
-  foreach my $cond (@ieConditionals) {
-    $text =~ s/\0ie$index/$cond/;
-  }
-
-  return $text unless @jsUrls;
-
-  # check if there's a cache file already
-  my ($cacheFileName, $cacheUrl) = getCacheEntry("js", \@jsUrls);
-
-  my $query = Foswiki::Func::getCgiQuery();
-  my $refresh = $query->param("refresh") || '';
-
-  if (DEBUG || $refresh =~ /\b(on|all|cache|js)\b/ || !-f $cacheFileName) {    
-    # TODO: compare timestamps of files
-    writeDebug("creating cache at $cacheFileName");
-
-    my $cachedData = '';
-
-    foreach my $url (@jsUrls) {
-      writeDebug("url=$url");
-      my $fileName = url2FileName($url);
-      if (-f $fileName) {
-        my $data = Foswiki::Func::readFile($fileName);
-        if ($data) {
-          $cachedData .= "\n\n/* DEBUG: fileName=$fileName */\n"
-            if DEBUG;
-          $cachedData .= $data;
-        }
-      } else {
-        print STDERR "woops $fileName does not exist\n";
-      }
+        writeDebug("found ie conditional: $1");
+        push @ieConditionals, $1;
+        $index++;
     }
 
-    # save the cached javascript
-    Foswiki::Func::saveFile($cacheFileName, $cachedData);
+    # collect all javascript
+    my @jsUrls = ();
+    my $excludePattern =
+      '(?!.*' . $Foswiki::cfg{PageOptimizerPlugin}{ExcludeJavaScript} . ')'
+      if defined $Foswiki::cfg{PageOptimizerPlugin}{ExcludeJavaScript};
 
-    # create a gzip'ed version as well
-    $cachedData = Compress::Zlib::memGzip($cachedData);
-    Foswiki::Func::saveFile($cacheFileName . '.gz', $cachedData);
+    while ( $text =~
+        s/<script .*?src=["'](\/$excludePattern[^"']+)["'].*><\/script>/\0js\0/
+      )
+    {
+        push @jsUrls, $1;
+    }
 
-    logJavaScript(\@jsUrls);
-  }
+    # put back ie stuff
+    $index = 0;
+    foreach my $cond (@ieConditionals) {
+        $text =~ s/\0ie$index/$cond/;
+    }
 
+    return $text unless @jsUrls;
 
-  # insert the cached javascript at the first position we've found a js url
-  $text =~ s/\0js\0/<script src='$cacheUrl'><\/script>/;
+    # check if there's a cache file already
+    my ( $cacheFileName, $cacheUrl ) = getCacheEntry( "js", \@jsUrls );
 
-  # remove the rest
-  $text =~ s/\0js\0//g;
+    my $query = Foswiki::Func::getCgiQuery();
+    my $refresh = $query->param("refresh") || '';
 
-  return $text;
+    if ( DEBUG || $refresh =~ /\b(on|all|cache|js)\b/ || !-f $cacheFileName ) {
+
+        # TODO: compare timestamps of files
+        writeDebug("creating cache at $cacheFileName");
+
+        my $cachedData = '';
+
+        foreach my $url (@jsUrls) {
+            writeDebug("url=$url");
+            my $fileName = url2FileName($url);
+            if ( -f $fileName ) {
+                my $data = Foswiki::Func::readFile($fileName);
+                if ($data) {
+                    $cachedData .= "\n\n/* DEBUG: fileName=$fileName */\n"
+                      if DEBUG;
+                    $cachedData .= $data;
+                }
+            }
+            else {
+                print STDERR "woops $fileName does not exist\n";
+            }
+        }
+
+        # save the cached javascript
+        Foswiki::Func::saveFile( $cacheFileName, $cachedData );
+
+        # create a gzip'ed version as well
+        $cachedData = Compress::Zlib::memGzip($cachedData);
+        Foswiki::Func::saveFile( $cacheFileName . '.gz', $cachedData );
+
+        logJavaScript( \@jsUrls );
+    }
+
+    # insert the cached javascript at the first position we've found a js url
+    $text =~ s/\0js\0/<script src='$cacheUrl'><\/script>/;
+
+    # remove the rest
+    $text =~ s/\0js\0//g;
+
+    return $text;
 }
 
 ###############################
 sub optimizeStylesheets {
-  my $text = shift;
+    my $text = shift;
 
-  # collect all css
-  my @cssUrls = ();
+    # collect all css
+    my @cssUrls = ();
 
-  $text =~ s/(?:<(link)\s+rel=["']stylesheet["']\s+href=["'](\/[^"']+)["']\s+media=["']all["'][^\/>]*?\/?>)|(?:<(style)\s+.*?media=["']all["'][^>]*?>(.*?)<\/style>)/_gatherCssUrls(\@cssUrls, $1||$3, $2||$4)/ges;
+    $text =~
+s/(?:<(link)\s+rel=["']stylesheet["']\s+href=["'](\/[^"']+)["']\s+media=["']all["'][^\/>]*?\/?>)|(?:<(style)\s+.*?media=["']all["'][^>]*?>(.*?)<\/style>)/_gatherCssUrls(\@cssUrls, $1||$3, $2||$4)/ges;
 
-  return $text unless @cssUrls;
+    return $text unless @cssUrls;
 
-  my ($cacheFileName, $cacheUrl) = getCacheEntry("css", \@cssUrls);
+    my ( $cacheFileName, $cacheUrl ) = getCacheEntry( "css", \@cssUrls );
 
-  my $query = Foswiki::Func::getCgiQuery();
-  my $refresh = $query->param("refresh") || '';
+    my $query = Foswiki::Func::getCgiQuery();
+    my $refresh = $query->param("refresh") || '';
 
-  if (DEBUG || $refresh =~ /\b(on|all|cache|css)\b/ || !-f $cacheFileName) {    
-    # TODO: compare timestamps of files
-    writeDebug("creating cache at $cacheFileName");
+    if ( DEBUG || $refresh =~ /\b(on|all|cache|css)\b/ || !-f $cacheFileName ) {
 
-    my $cachedData = '';
+        # TODO: compare timestamps of files
+        writeDebug("creating cache at $cacheFileName");
 
-    foreach my $url (@cssUrls) {
-      my $data = parseStylesheet($url);
-      $cachedData .= $data if $data;
+        my $cachedData = '';
+
+        foreach my $url (@cssUrls) {
+            my $data = parseStylesheet($url);
+            $cachedData .= $data if $data;
+        }
+
+        Foswiki::Func::saveFile( $cacheFileName, $cachedData );
+        $cachedData = Compress::Zlib::memGzip($cachedData);
+        Foswiki::Func::saveFile( $cacheFileName . '.gz', $cachedData );
+
+        logStylesheet( \@cssUrls );
     }
 
-    Foswiki::Func::saveFile($cacheFileName, $cachedData);
-    $cachedData = Compress::Zlib::memGzip($cachedData);
-    Foswiki::Func::saveFile($cacheFileName . '.gz', $cachedData);
+    $text =~ s/\0css\0/<link rel='stylesheet' href='$cacheUrl' media='all' \/>/;
+    $text =~ s/\0css\0//g;
 
-    logStylesheet(\@cssUrls);
-  }
-
-  $text =~ s/\0css\0/<link rel='stylesheet' href='$cacheUrl' media='all' \/>/;
-  $text =~ s/\0css\0//g;
-
-  return $text;
+    return $text;
 }
 
 ###############################
 sub _gatherCssUrls {
-  my ($cssUrls, $type, $data) = @_;
+    my ( $cssUrls, $type, $data ) = @_;
 
-  # link tag
-  if ($type eq 'link') {
-    my $excludePattern = $Foswiki::cfg{PageOptimizerPlugin}{ExcludeCss}
-      if defined $Foswiki::cfg{PageOptimizerPlugin}{ExcludeCss};
+    # link tag
+    if ( $type eq 'link' ) {
+        my $excludePattern = $Foswiki::cfg{PageOptimizerPlugin}{ExcludeCss}
+          if defined $Foswiki::cfg{PageOptimizerPlugin}{ExcludeCss};
 
-    if ($data =~ /$excludePattern/) {
-      return "<style media='all'>$data</style>";
+        if ( $data =~ /$excludePattern/ ) {
+            return "<style media='all'>$data</style>";
+        }
+
+        #writeDebug("found url $data in link tag");
+        push @$cssUrls, $data;
+        return "\0css\0";
     }
 
-    #writeDebug("found url $data in link tag");
-    push @$cssUrls, $data;
-    return "\0css\0";
-  }
+    # style block
+    while ( $data =~ s/\@import +url\(["']?(.*?)["']?\);// ) {
+        push @$cssUrls, $1;
+        writeDebug("found url $1 in style block");
+    }
 
-  # style block
-  while ($data =~ s/\@import +url\(["']?(.*?)["']?\);//) {
-    push @$cssUrls, $1;
-    writeDebug("found url $1 in style block");
-  }
+    # test for an empty style block
+    if ( $data =~ /^\s*$/s ) {
+        return "\0css\0";
+    }
 
-  # test for an empty style block
-  if ($data =~ /^\s*$/s) {
-    return "\0css\0";
-  }
-
-  # return the rest of the style block
-  return "\0css\0<style media='all'>$data</style>";
+    # return the rest of the style block
+    return "\0css\0<style media='all'>$data</style>";
 }
 
 ###############################
 sub parseStylesheet {
-  my ($url, $baseUrl) = @_;
+    my ( $url, $baseUrl ) = @_;
 
-  $baseUrl ||= $url;
-  $baseUrl =~ s/\?.*?$//;
+    $baseUrl ||= $url;
+    $baseUrl =~ s/\?.*?$//;
 
-  my $fileName = url2FileName($url);
+    my $fileName = url2FileName($url);
 
-  #writeDebug("baseUrl=$baseUrl");
-  #writeDebug("fileName=$fileName");
+    #writeDebug("baseUrl=$baseUrl");
+    #writeDebug("fileName=$fileName");
 
-  my $data = '';
+    my $data = '';
 
-  if (-f $fileName) {
-    $data = Foswiki::Func::readFile($fileName);
+    if ( -f $fileName ) {
+        $data = Foswiki::Func::readFile($fileName);
 
-    $data =~ s/url\(["']?(.*?)["']?\)/"url(".rewriteUrl($1, $baseUrl).")"/ge;
-    $data =~ s/\@import +url\(["']?(.*?)["']?\);/parseStylesheet($1)/ge;
+        $data =~
+          s/url\(["']?(.*?)["']?\)/"url(".rewriteUrl($1, $baseUrl).")"/ge;
+        $data =~ s/\@import +url\(["']?(.*?)["']?\);/parseStylesheet($1)/ge;
 
-  } else {
-    print STDERR "woops $fileName does not exist\n";
-  }
+    }
+    else {
+        print STDERR "woops $fileName does not exist\n";
+    }
 
-  return $data;
+    return $data;
 }
 
 ###############################
 sub purgeCache {
 
-  my $cacheDir = $Foswiki::cfg{PubDir} . '/' . $Foswiki::cfg{SystemWebName} . '/' . $pluginName . '/cache/';
+    my $cacheDir =
+        $Foswiki::cfg{PubDir} . '/'
+      . $Foswiki::cfg{SystemWebName} . '/'
+      . $pluginName
+      . '/cache/';
 
-  opendir(my $dh, $cacheDir);
-  my @files = map { Foswiki::Sandbox::normalizeFileName($cacheDir . '/' . $_) } grep { !/^(\.|README)/ } readdir $dh;
-  closedir $dh;
+    opendir( my $dh, $cacheDir );
+    my @files =
+      map  { Foswiki::Sandbox::normalizeFileName( $cacheDir . '/' . $_ ) }
+      grep { !/^(\.|README)/ } readdir $dh;
+    closedir $dh;
 
-  #writeDebug("cleaning up @files");
-  unlink @files;
+    #writeDebug("cleaning up @files");
+    unlink @files;
 }
 
 ###############################
 sub getCacheEntry {
-  my ($type, $urls) = @_;
+    my ( $type, $urls ) = @_;
 
-  my $fileName = Digest::MD5::md5_hex(@$urls) . '.' . $type;
+    my $fileName = Digest::MD5::md5_hex(@$urls) . '.' . $type;
 
-  my $cacheDir = $Foswiki::cfg{PubDir} . '/' . $Foswiki::cfg{SystemWebName} . '/' . $pluginName . '/cache/';
+    my $cacheDir =
+        $Foswiki::cfg{PubDir} . '/'
+      . $Foswiki::cfg{SystemWebName} . '/'
+      . $pluginName
+      . '/cache/';
 
-  mkdir $cacheDir unless -d $cacheDir;
+    mkdir $cacheDir unless -d $cacheDir;
 
-  my $cachePath = $Foswiki::cfg{PubUrlPath} . '/' . $Foswiki::cfg{SystemWebName} . '/' . $pluginName . '/cache/';
+    my $cachePath =
+        $Foswiki::cfg{PubUrlPath} . '/'
+      . $Foswiki::cfg{SystemWebName} . '/'
+      . $pluginName
+      . '/cache/';
 
-  return ($cacheDir . $fileName, $cachePath . $fileName);
+    return ( $cacheDir . $fileName, $cachePath . $fileName );
 }
 
 ###############################
 sub url2FileName {
-  my $url = shift;
+    my $url = shift;
 
-  my $fileName = $url;
-  $fileName =~ s/$Foswiki::cfg{PubUrlPath}/$Foswiki::cfg{PubDir}/;
-  $fileName =~ s/^$Foswiki::cfg{DefaultUrlHost}//;
-  $fileName =~ s/\?.*$//;
+    my $fileName = $url;
+    $fileName =~ s/$Foswiki::cfg{PubUrlPath}/$Foswiki::cfg{PubDir}/;
+    $fileName =~ s/^$Foswiki::cfg{DefaultUrlHost}//;
+    $fileName =~ s/\?.*$//;
 
-  return $fileName;
+    return $fileName;
 }
 
 ###############################
 sub rewriteUrl {
-  my ($url, $baseUrl) = @_;
+    my ( $url, $baseUrl ) = @_;
 
-  return $url if $url =~ /^(data|https?):/;
+    return $url if $url =~ /^(data|https?):/;
 
-  #writeDebug("rewriteUrl($url, $baseUrl)");
+    #writeDebug("rewriteUrl($url, $baseUrl)");
 
-  my $uri = URI->new($url);
-  $url = $uri->abs($baseUrl);
+    my $uri = URI->new($url);
+    $url = $uri->abs($baseUrl);
 
-  #writeDebug("... url=$url");
+    #writeDebug("... url=$url");
 
-  return $url;
+    return $url;
 }
 
 ###############################
 sub logJavaScript {
 
-  return unless $Foswiki::cfg{PageOptimizerPlugin}{GatherStatistics};
+    return unless $Foswiki::cfg{PageOptimizerPlugin}{GatherStatistics};
 
-  require Foswiki::Plugins::PageOptimizerPlugin::Stats;
-  Foswiki::Plugins::PageOptimizerPlugin::Stats::logJavaScript(@_);
+    require Foswiki::Plugins::PageOptimizerPlugin::Stats;
+    Foswiki::Plugins::PageOptimizerPlugin::Stats::logJavaScript(@_);
 }
 
 ###############################
 sub logStylesheet {
 
-  return unless $Foswiki::cfg{PageOptimizerPlugin}{GatherStatistics};
-  
-  require Foswiki::Plugins::PageOptimizerPlugin::Stats;
-  Foswiki::Plugins::PageOptimizerPlugin::Stats::logStylesheet(@_);
-}
+    return unless $Foswiki::cfg{PageOptimizerPlugin}{GatherStatistics};
 
+    require Foswiki::Plugins::PageOptimizerPlugin::Stats;
+    Foswiki::Plugins::PageOptimizerPlugin::Stats::logStylesheet(@_);
+}
 
 1;
--- lib/Foswiki/Plugins/PageOptimizerPlugin/Stats.pm
+++ lib/Foswiki/Plugins/PageOptimizerPlugin/Stats.pm
@@ -18,115 +18,120 @@ package Foswiki::Plugins::PageOptimizerPlugin::Stats;
 use strict;
 use warnings;
 
-use Foswiki::Func ();
+use Foswiki::Func    ();
 use Foswiki::Plugins ();
 our $pluginName = 'PageOptimizerPlugin';
 
 ###############################
 sub logJavaScript {
-  my $urls = shift;
+    my $urls = shift;
 
-  my $workArea = Foswiki::Func::getWorkArea($pluginName);
-  my $fileName = $workArea.'/javascript.log';
-  my $file;
+    my $workArea = Foswiki::Func::getWorkArea($pluginName);
+    my $fileName = $workArea . '/javascript.log';
+    my $file;
 
-  my $baseWeb = $Foswiki::Plugins::SESSION->{webName};
-  my $baseTopic = $Foswiki::Plugins::SESSION->{topicName};
+    my $baseWeb   = $Foswiki::Plugins::SESSION->{webName};
+    my $baseTopic = $Foswiki::Plugins::SESSION->{topicName};
 
-  open($file, ">>", $fileName) or die "can't open logfile $fileName";
-  print $file "$baseWeb.$baseTopic:" . join(', ', @$urls) . "\n";
-  close($file);
+    open( $file, ">>", $fileName ) or die "can't open logfile $fileName";
+    print $file "$baseWeb.$baseTopic:" . join( ', ', @$urls ) . "\n";
+    close($file);
 }
 
 ###############################
 sub logStylesheet {
-  my $urls = shift;
+    my $urls = shift;
 
-  my $workArea = Foswiki::Func::getWorkArea($pluginName);
-  my $fileName = $workArea.'/stylesheet.log';
-  my $file;
+    my $workArea = Foswiki::Func::getWorkArea($pluginName);
+    my $fileName = $workArea . '/stylesheet.log';
+    my $file;
 
-  my $baseWeb = $Foswiki::Plugins::SESSION->{webName};
-  my $baseTopic = $Foswiki::Plugins::SESSION->{topicName};
+    my $baseWeb   = $Foswiki::Plugins::SESSION->{webName};
+    my $baseTopic = $Foswiki::Plugins::SESSION->{topicName};
 
-  open($file, ">>", $fileName) or die "can't open logfile $fileName";
-  print $file "$baseWeb.$baseTopic:" . join(', ', @$urls) . "\n";
-  close($file);
+    open( $file, ">>", $fileName ) or die "can't open logfile $fileName";
+    print $file "$baseWeb.$baseTopic:" . join( ', ', @$urls ) . "\n";
+    close($file);
 }
 
 ###############################
 sub restStatistics {
-  my ($session, $subject, $verb, $response) = @_;
+    my ( $session, $subject, $verb, $response ) = @_;
 
-  #print STDERR "called restStatistics\n";
+    #print STDERR "called restStatistics\n";
 
-  my $query = Foswiki::Func::getCgiQuery();
-  my $type = $query->param("type");
+    my $query = Foswiki::Func::getCgiQuery();
+    my $type  = $query->param("type");
 
-  my $workArea = Foswiki::Func::getWorkArea($pluginName);
+    my $workArea = Foswiki::Func::getWorkArea($pluginName);
 
-  if (!defined $type || $type eq 'js') {
-    my $fileName = $workArea.'/javascript.log';
-    my $stats = gatherStatistics($fileName);
-    if ($stats) {
-      print "javascripts: ".scalar(keys %$stats)."\n";
-      foreach my $key (sort {$stats->{$b} <=> $stats->{$a}} keys %$stats) {
-        my $val = int($stats->{$key} * 100);
-        print "   $key: $val%\n";
-      }
+    if ( !defined $type || $type eq 'js' ) {
+        my $fileName = $workArea . '/javascript.log';
+        my $stats    = gatherStatistics($fileName);
+        if ($stats) {
+            print "javascripts: " . scalar( keys %$stats ) . "\n";
+            foreach
+              my $key ( sort { $stats->{$b} <=> $stats->{$a} } keys %$stats )
+            {
+                my $val = int( $stats->{$key} * 100 );
+                print "   $key: $val%\n";
+            }
+        }
     }
-  }
 
-  if (!defined $type || $type eq 'css') {
-    my $fileName = $workArea.'/stylesheet.log';
-    my $stats = gatherStatistics($fileName);
-    if ($stats) {
-      print "stylesheets: ".scalar(keys %$stats)."\n";
-      foreach my $key (sort {$stats->{$b} <=> $stats->{$a}} keys %$stats) {
-        my $val = int($stats->{$key} * 100);
-        print "   $key: $val%\n";
-      }
+    if ( !defined $type || $type eq 'css' ) {
+        my $fileName = $workArea . '/stylesheet.log';
+        my $stats    = gatherStatistics($fileName);
+        if ($stats) {
+            print "stylesheets: " . scalar( keys %$stats ) . "\n";
+            foreach
+              my $key ( sort { $stats->{$b} <=> $stats->{$a} } keys %$stats )
+            {
+                my $val = int( $stats->{$key} * 100 );
+                print "   $key: $val%\n";
+            }
+        }
     }
-  }
 
-  return "";
+    return "";
 }
 
 ###############################
 sub gatherStatistics {
-  my ($fileName) = @_;
+    my ($fileName) = @_;
 
-  #print STDERR "called gatherStatistics\n";
+    #print STDERR "called gatherStatistics\n";
 
-  unless (-e $fileName) {
-    print STDERR "no statistics found in $fileName\n";
-    return;
-  }
-
-  my $file;
-  my $data = Foswiki::Func::readFile($fileName);
-  my %stats = ();
-
-  my $index = 0;
-  while($data =~ /^(.*?)$/mg) {
-    my $line = $1;
-    if ($line =~ /^(.*):\s*(.*)$/) {
-      my $topic = $1;
-      $index++;
-      #print STDERR "topic=$topic\n";
-      foreach my $url (split(/\s*,\s*/, $2)) {
-        my $key = $url;
-        $key =~ s/\?.*?$//;
-        push @{$stats{$key}}, $topic;
-      }
+    unless ( -e $fileName ) {
+        print STDERR "no statistics found in $fileName\n";
+        return;
     }
-  }
 
-  foreach my $key (keys %stats) {
-    $stats{$key} = (scalar(@{$stats{$key}} + 0.0) / $index);
-  }
+    my $file;
+    my $data  = Foswiki::Func::readFile($fileName);
+    my %stats = ();
 
-  return \%stats;
+    my $index = 0;
+    while ( $data =~ /^(.*?)$/mg ) {
+        my $line = $1;
+        if ( $line =~ /^(.*):\s*(.*)$/ ) {
+            my $topic = $1;
+            $index++;
+
+            #print STDERR "topic=$topic\n";
+            foreach my $url ( split( /\s*,\s*/, $2 ) ) {
+                my $key = $url;
+                $key =~ s/\?.*?$//;
+                push @{ $stats{$key} }, $topic;
+            }
+        }
+    }
+
+    foreach my $key ( keys %stats ) {
+        $stats{$key} = ( scalar( @{ $stats{$key} } + 0.0 ) / $index );
+    }
+
+    return \%stats;
 }
 
 1;
